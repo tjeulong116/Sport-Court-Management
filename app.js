dff0c5941d08923ca2140327f54e87566c9fefc2
hieubk <tjeuong116@gmail.com>
2025-11-12 20:52:46 +0700

    video 24

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-let a = 5;
-let b = 10;
-console.log(`sum = ${a + b}`);
+const express = require("express")
+const app = express()
+const PORT = 8080
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.get("/hoidanit", (req, res) => {
+    res.send("Hello Hieu")
+})
+
+app.listen(PORT, () => {
+    console.log("My app is running ok")
+})
