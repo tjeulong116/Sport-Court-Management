f79e832d126d69ce15872a215e149ba87788de54
hieubk <tjeuong116@gmail.com>
2025-11-13 11:35:21 +0700

    video 30

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 const express = require("express")
+require('dotenv').config()
+
 const app = express()
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
     res.send("Hello World nodemon")
@@ -10,6 +12,7 @@ app.get("/hoidanit", (req, res) => {
     res.send("Hello Hieu")
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log("My app is running ok")
+    console.log(`env port: + ${process.env.PORT}`)
 })
