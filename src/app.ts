230aedb391bea701552158f31e40b0806a576d43
hieubk <tjeuong116@gmail.com>
2025-11-13 11:48:50 +0700

    video 31

--- src/app.ts
+++ src/app.ts
@@ -5,14 +5,18 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-    res.send("Hello World nodemon")
+    res.send(`<h1 style = "color: red">Hello World nodemon</h1>`)
 })
 
 app.get("/hoidanit", (req, res) => {
     res.send("Hello Hieu")
 })
 
+app.get("/abc", (req, res) => {
+    res.send("Hello abc")
+})
+
 app.listen(process.env.PORT, () => {
     console.log("My app is running ok")
-    console.log(`env port: + ${process.env.PORT}`)
+    console.log(`env port: ${process.env.PORT}`)
 })
