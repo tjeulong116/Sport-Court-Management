fcb9f6288273bc2cc9cc5bc16c3fa99e271c1a84
hieubk <tjeuong116@gmail.com>
2025-11-14 10:25:09 +0700

    video 39

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,22 @@ import express from 'express';
 import 'dotenv/config';
 import webRoutes from "./routes/web";
 
-const app = express()
-const PORT = process.env.PORT || 8080
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 //config view engine
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-//config routes
-webRoutes(app);
+//config req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //config static files : css/images/js
 app.use(express.static('public'));
+
+//config routes
+webRoutes(app);
 
 app.listen(process.env.PORT, () => {
     console.log("My app is running ok");
